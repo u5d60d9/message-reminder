bf5e1d0e9829cdf964b245b2f25c603f7069c39b
刘玮 <liuwei@mingyuanyun.com>
2018-08-02 16:29:08 +0800

    2018-08-02 tryicon fixed & autoupdate

--- main.js
+++ main.js
@@ -1,48 +1,55 @@
 // Modules to control application life and create native browser window
-const version = "0.0.1.0"
+const { autoUpdater } = require("electron-updater")
 const path = require('path')
-const electron = require('electron') 
-const {app, dialog, Menu, Tray, BrowserWindow,Notification,ipcRenderer,ipcMain}  = electron
+const electron = require('electron')
+const { app, dialog, Menu, Tray, BrowserWindow, Notification, ipcRenderer, ipcMain, nativeImage } = electron
 const io = require('socket.io-client');
 var toml = require('toml');
 var fs = require('fs');
 var tomlify = require('tomlify-j0.4');
 
 app.setName("消息提醒程序")
-const programPath = (electron.app || electron.remote.app).getAppPath();
+
+autoUpdater.on('update-downloaded', (ev, info) => {
+  var alert_msg = { title: "新版本下载完毕", timestamp: new Date().toLocaleTimeString(), content: "新版本将在程序退出时安装！" };
+    alertWindow.webContents.send('alert-msg', alert_msg);
+    alertWindow.show();
+})
+
+ const programPath = (electron.app || electron.remote.app).getAppPath();
 const dataPath = (electron.app || electron.remote.app).getPath('userData');
 
-const appFileUrl =  path.join(programPath, 'resources/config/app.config');
+const appFileUrl = path.join(programPath, 'resources/config/app.config');
 const configfileUrl = path.join(dataPath, 'app.config');
 //  let contentText;
- let appconfig; 
+let appconfig;
 //copy installer to userdata
-if(fs.existsSync(configfileUrl)) {
-//  var fse = require('fs-extra');
-//   fse.copySync(appFileUrl,configfileUrl)
- var contentText=fs.readFileSync(configfileUrl);
+if (fs.existsSync(configfileUrl)) {
+  //  var fse = require('fs-extra');
+  //   fse.copySync(appFileUrl,configfileUrl)
+  var contentText = fs.readFileSync(configfileUrl);
   appconfig = toml.parse(contentText)
-}else{
+} else {
   //default config 
   appconfig = {
-    login:{
+    login: {
       // server:"http://10.5.10.6:3000",
       // server:"http://121.43.163.226:3050",
-       server:"http://sol-msg.fdcyun.com:4000",
-      user:""
+      server: "http://sol-msg.fdcyun.com:4000",
+      user: ""
     },
-    options:{
+    options: {
       blink_on_message: true,
-      alert_on_message : true,
-      keep_history :false
+      alert_on_message: true,
+      keep_history: false
     }
   }
 }
 
-const normal_icon='resources/assets/icon/favicon.png'
-const blink_icon =  "resources/assets/icon/favicon.ico"
-// const normal_icon= path.join(programPath, 'resources/assets/icon/favicon.ico');
-// const blink_icon = path.join(programPath, "resources/assets/icon/favicon1.png")
+const normal_icon = "resources/assets/icon/favicon.png"
+const normal_iconPath =  path.resolve(__dirname,'assets','icon','favicon.png')
+const blink_icon = 'resources/assets/icon/favicon.ico';
+const blink_iconPath =  path.resolve(__dirname,'assets','icon','favicon.ico')
 
 let uid //login id
 let mainWindow
@@ -54,14 +61,14 @@ let server;
 // let lastmsg;
 
 
-  server=appconfig.login.server;
-  uid = appconfig.login.user;
+server = appconfig.login.server;
+uid = appconfig.login.user;
 
 const socket = io(server);
 var template = [
   {
     label: '关闭',
-    click: function () { app.exit() },    
+    click: function () { app.exit() },
     // submenu: [
     //   {
     //     label: 'Undo',
@@ -71,58 +78,64 @@ var template = [
     // ]
   },
   {
-    label:'版本',
-    click:()=>{ 
-          mainWindow.webContents.executeJavaScript("alert('"+ "版本:V"+version +"');");
-    //  dialog.showMessageBox({type:"info",title:"信息",message:"版本:v"+version,buttons:[]}) 
+    label: '版本',
+    click: () => {
+      mainWindow.webContents.executeJavaScript("alert('" + "版本:V" + app.getVersion() + "');");
+      //  dialog.showMessageBox({type:"info",title:"信息",message:"版本:v"+version,buttons:[]}) 
     }
   }
-  ]
+]
 var mainMenu = Menu.buildFromTemplate(template)
 Menu.setApplicationMenu(mainMenu);
 
 
-ipcMain.on('startfill', (event, arg)=> {    
-    event.sender.send("fill",appconfig.login.user);
+ipcMain.on('startfill', (event, arg) => {
+  event.sender.send("fill", appconfig.login.user);
 })
 
 
 
-ipcMain.on('login', (event, arg)=> {
-    login(arg);
+ipcMain.on('login', (event, arg) => {
+  login(arg);
 })
 
 
-function login(arg){
-//add stuff
+function login(arg) {
+  //add stuff
   appconfig.login.user = arg;
   uid = arg;
   socket.emit("set-name", uid);
 
-  fs.writeFile(configfileUrl,tomlify.toToml(appconfig));
+  fs.writeFile(configfileUrl, tomlify.toToml(appconfig));
 
-
-  console.log(arg);
   mainWindow.hide()
 }
 
-function stopBlinkImage () {
-    // dialog.showMessageBox({type:"info",title:"信息",message:"stop"});
-
+function stopBlinkImage() {
+  // dialog.showMessageBox({type:"info",title:"信息",message:"stop"});
+  try {
     if (blinker !== null) {
-        clearInterval(blinker);
-        blinker = null;
+      clearInterval(blinker);
+      blinker = null;
     }
-    tray.setImage(normal_icon);
+    tray.setImage(normal_iconPath);
+  } catch (e) {
+
+  }
 }
 
-function startBlinkImage (title) {
-  if(blinker==null){
-    let on = false;
-    blinker = setInterval(function() {
-        tray.setImage(on ? title : normal_icon);
+function startBlinkImage() {
+  try {
+    if (blinker == null) {
+      let on = false;
+      blinker = setInterval(function () {
+        var icon = nativeImage.createFromPath(on ? blink_iconPath : normal_iconPath);
+        tray.setImage(icon);
         on = !on;
-    }, 500);
+      }, 500);
+    }
+  } catch (e) {
+
   }
 }
 
@@ -134,43 +147,48 @@ function createWindow() {
       height: 240,
       frame: true,
       // skipTaskbar:true,
-      alwaysOnTop:true,
-      title:"消息提醒程序"
+      alwaysOnTop: true,
+      title: "消息提醒程序",
+      show: false
     }
   )
-   
-alertWindow = new BrowserWindow(
-  {
-      width:422,
-      height:158,
-    // useContentSize:true,
-    frame:false,
-    resizable:false,
-    alwaysOnTop:true,
-    focusable:false,
-    skipTaskbar:true,
-    movable:false,
-    show: false,
-    backgroundColor: '#2e2c29'
-  }
-)
+
+  mainWindow.once('ready-to-show', () => {
+    mainWindow.show()
+  })
+
+  alertWindow = new BrowserWindow(
+    {
+      width: 422,
+      height: 158,
+      // useContentSize:true,
+      frame: false,
+      resizable: false,
+      alwaysOnTop: true,
+      focusable: false,
+      skipTaskbar: true,
+      movable: false,
+      show: false,
+      backgroundColor: '#2e2c29'
+    }
+  )
 
   historyWindow = new BrowserWindow(
-    {          
-      width:800,
-      height:600,
-      skipTaskbar:true,
-      alwaysOnTop:true,
+    {
+      width: 800,
+      height: 600,
+      skipTaskbar: true,
+      alwaysOnTop: true,
       show: false,
-      resizable:false,
-      title:"查看消息"
+      resizable: false,
+      title: "查看消息"
     }
   )
 
- alertWindow.loadFile('alert.html')
+  alertWindow.loadFile('alert.html')
 
- var area = electron.screen.getPrimaryDisplay().workAreaSize
- alertWindow.setPosition(area.width-442,area.height-174)
+  var area = electron.screen.getPrimaryDisplay().workAreaSize
+  alertWindow.setPosition(area.width - 442, area.height - 174)
 
   // and load the index.html of the app.
   mainWindow.loadFile('index.html')
@@ -178,49 +196,45 @@ alertWindow = new BrowserWindow(
   // mainWindow.flashFrame(true)
   historyWindow.loadFile('history.html')
   // Open the DevTools.
-  // historyWindow.webContents.openDevTools()
-   mainWindow.on('close', function (e) {
-     mainWindow.hide()
-     e.preventDefault()
-   })
+  //  mainWindow.webContents.openDevTools()
+  mainWindow.on('close', function (e) {
+    mainWindow.hide()
+    e.preventDefault()
+  })
   historyWindow.on('close', function (e) {
-     historyWindow.hide()
-     e.preventDefault()
-   })
-  alertWindow.on("click",()=>{alertWindow.hide();})
-  ipcMain.on("alert-close",(evt, arg) => {
+    historyWindow.hide()
+    e.preventDefault()
+  })
+  alertWindow.on("click", () => { alertWindow.hide(); })
+  ipcMain.on("alert-close", (evt, arg) => {
     alertWindow.hide();
     stopBlinkImage();
   })
 
   // ipcMain.on('alert-ready', (evt, arg) => {
-  //       console.log(lastmsg)
   //       var date = new Date().toLocaleTimeString() ;
   //       evt.sender.send('alert-msg', lastmsg);
   //   });
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
     mainWindow = null
   })
 }
 
 //单例~BEGIN
 const shouldQuit = app.makeSingleInstance(
-        (commandLine, workingDirectory) => {
-            if (mainWindow) {
-                if (mainWindow.isMinimized()){
-                     mainWindow.restore();
-                }else if(!mainWindow.isVisible){
-                    mainWindow.show();
-                };
-            mainWindow.focus();
-         };
-   });
-   
+  (commandLine, workingDirectory) => {
+    if (mainWindow) {
+      if (mainWindow.isMinimized()) {
+        mainWindow.restore();
+      } else if (!mainWindow.isVisible) {
+        mainWindow.show();
+      };
+      mainWindow.focus();
+    };
+  });
+
 if (shouldQuit) {
   app.quit();
   return;
@@ -232,53 +246,61 @@ if (shouldQuit) {
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 // app.on('ready', createWindow)
-
+app.on('window-all-closed', () => {
+  app.quit();
+});
 app.on('ready', () => {
-      // console.log(electron.screen.getPrimaryDisplay().workAreaSize)
+  try {
+ 
+    tray = new Tray(nativeImage.createFromPath(normal_iconPath));
+    // tray = new Tray(nativeImage.createFromPath(normal_iconPath));  
+    //tray = new Tray(normal_icon)
+  } catch (e) {
 
-  tray = new Tray(normal_icon)
+  }
+
   const contextMenu = Menu.buildFromTemplate([
-    {label: '登录窗口',click:()=>{mainWindow.show();} },
-    {label: '闪烁图标',type:"checkbox",checked:true ,enabled:appconfig.options.blink_on_message },
-    {label: '弹出消息',type:"checkbox",checked:true, enabled:appconfig.options.alert_on_message},
-    {label: '保留历史',type:"checkbox",checked:true,enabled:appconfig.options.keep_history },
-    {label: '退出',click:function(evt) { app.exit();}}
+    { label: '登录窗口', click: () => { mainWindow.show(); } },
+    { label: '闪烁图标', type: "checkbox", checked: true, enabled: appconfig.options.blink_on_message },
+    { label: '弹出消息', type: "checkbox", checked: true, enabled: appconfig.options.alert_on_message },
+    { label: '保留历史', type: "checkbox", checked: true, enabled: appconfig.options.keep_history },
+    { label: '退出', click: function (evt) { app.exit(); } }
   ])
 
   tray.setToolTip('消息提醒')
   tray.setContextMenu(contextMenu)
-  tray.on("click",stopBlinkImage)
-  tray.on("double-click",()=>{
+  tray.on("click", stopBlinkImage)
+  tray.on("double-click", () => {
     historyWindow.webContents.send("display-history-msg");
     historyWindow.show()
   })
-  
+
   //init socketio 
   socket.emit("set-name", uid);
-  socket.on("msg",function(data){ 
+  socket.on("msg", function (data) {
     //tray.displayBalloon({title:data.cst+" " + date,content:data.msg})  
-    
-    
-    
-      var date = new Date().toLocaleTimeString() ;
-     // evt.sender.send('alert-msg', {title:data.cst+" " + date,content:data.msg})
-     var alert_msg={title:data.cst,timestamp:date,content:data.msg};
+
+
+
+    var date = new Date().toLocaleTimeString();
+    // evt.sender.send('alert-msg', {title:data.cst+" " + date,content:data.msg})
+    var alert_msg = { title: data.cst, timestamp: date, content: data.msg };
     alertWindow.webContents.send('alert-msg', alert_msg);
     historyWindow.webContents.send('alert-msg', alert_msg);
     //如果是打开状态，刷新内容
-    if(historyWindow.isVisible){
-        historyWindow.webContents.send("display-history-msg");
+    if (historyWindow.isVisible) {
+      historyWindow.webContents.send("display-history-msg");
     }
     // lastmsg =  {title:data.cst+" " + date,content:data.msg};
     // alertWindow.reload();
     alertWindow.show();
 
-    startBlinkImage(blink_icon)
+    startBlinkImage()
   })
   socket.on('reconnect', (attemptNumber) => {
     socket.emit("set-name", uid);
   });
-
+  autoUpdater.checkForUpdatesAndNotify();
   createWindow()
 })
 
